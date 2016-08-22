e660fbebad18f63fe4070c20a97646c10d5e2f40
Shan Jiang <shanjiang.developer@gmail.com>
2016-08-22 14:01:34 +0200

    try

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
+const token = process.env.FB_PAGE_ACCESS_TOKEN
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -11,7 +12,7 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is TestBot Server, jojo');
+    res.send('This is TestBot Server, tacktack');
 });
 
 // Facebook Webhook
@@ -23,36 +24,39 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-// handler receiving messages
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
+// Add an API endpoint to index.js to process messages. Remember to also include the token we got earlier.
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
     }
-    res.sendStatus(200);
-});
+    res.sendStatus(200)
+})
 
-// generic function sending messages
-function sendMessage(recipientId, message) {
+
+
+
+//Add a function to echo back messages
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        qs: {access_token:token},
         method: 'POST',
         json: {
-            recipient: {id: recipientId},
-            message: message,
+            recipient: {id:sender},
+            message: messageData,
         }
     }, function(error, response, body) {
         if (error) {
-            console.log('Error sending message: ', error);
+            console.log('Error sending messages: ', error)
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+            console.log('Error: ', response.body.error)
         }
-    });
-};
-
-
-
+    })
+}
